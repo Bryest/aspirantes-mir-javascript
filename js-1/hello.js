8733e1e3aa89b7bd4a0dd3a4a2637177a538043c
Renato <u201511892@upc.edu.pe>
2023-05-10 18:32:42 -0500

    feat: solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola(text) {
+    return text;
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
